27c223f7f35fb96040c2870db9775d140f63619b
Eli Perelman <eli@eliperelman.com>
2016-12-14 10:35:35 -0600

    Add total OKR score to title, style updates

--- src/components/Objective.js
+++ src/components/Objective.js
@@ -7,11 +7,29 @@ export default class extends React.Component {
 
   render() {
     const { title, keyResults, goals } = this.props;
+    let abs = 0;
+    let length = keyResults.length;
+    const totalScore = Math.round(100 * ((keyResults.reduce((score, keyResult) => {
+      if (!keyResult.score) {
+        return score;
+      }
+
+      if (keyResult.score.startsWith('+')) {
+        abs += parseFloat(keyResult.score);
+        length -= 1;
+        return score;
+      }
+
+      return score + parseFloat(keyResult.score);
+    }, 0) / length) + abs)) / 100;
 
     return (
       <div className="col-xs-12 col-md-4">
         <div className="objective">
-          <h2>{title}</h2>
+          <h2>
+            <Label type="info">{totalScore}</Label>
+            {title}
+          </h2>
           {goals.map((goal, key) => (
             <Label key={key}>{goal}</Label>
           ))}
